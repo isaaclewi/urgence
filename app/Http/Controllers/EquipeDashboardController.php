45e7e6ce7eb0cdf13e0444a45163ef577fac2f7a
Isaaclewi <lombaisaac8@gmail.com>
2026-06-09 22:08:27 +0200

    second commit

--- app/Http/Controllers/EquipeDashboardController.php
+++ app/Http/Controllers/EquipeDashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Services;
-use App\Models\Alertes;
+use App\Models\alertes;
 use App\Models\AlerteAffectation;
 
 class EquipeDashboardController extends Controller
