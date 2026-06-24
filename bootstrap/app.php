cdc30eb64d38133c76a3c54f0a752f134641d840
Isaaclewi <lombaisaac8@gmail.com>
2026-06-24 13:08:41 +0200

    finale

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth.citoyen' => \App\Http\Middleware\AuthCitoyen::class,
+            'auth.admin'   => \App\Http\Middleware\AuthAdmin::class,
+            'auth.service' => \App\Http\Middleware\AuthService::class,
+            'auth.equipe'  => \App\Http\Middleware\AuthEquipe::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
